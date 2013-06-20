6310376ed7fd69e10b851b5da5f008d1f9ecf7cb
Fernando Pinheiro <fpinheiro@inoa.com.br>
2013-06-20 10:48:39 -0300

    Adding progress bar to start screen and and scroll view for splash screen and item screen

--- my.catalog/gen/my/catalog/R.java
+++ my.catalog/gen/my/catalog/R.java
@@ -32,12 +32,14 @@ public final class R {
         public static final int navigation_next_item=0x7f020008;
     }
     public static final class id {
-        public static final int action_navigate=0x7f090007;
+        public static final int action_navigate=0x7f090009;
         public static final int gallery1=0x7f090006;
         public static final int imageView1=0x7f090000;
         public static final int list_image=0x7f090003;
+        public static final int progressBar1=0x7f090008;
         public static final int subtitle=0x7f090005;
         public static final int textView1=0x7f090001;
+        public static final int textView2=0x7f090007;
         public static final int thumbnail=0x7f090002;
         public static final int title=0x7f090004;
     }
@@ -58,8 +60,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int be_cool=0x7f060007;
         public static final int catalog_map=0x7f060005;
         public static final int hello_world=0x7f060002;
+        public static final int loading=0x7f060006;
         public static final int unzipButton=0x7f060003;
         public static final int up_tree=0x7f060004;
     }
